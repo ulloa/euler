db9f8b9e4e41a44a9c1c9dafa0617351e3a97cc0
ulloa <samuel.ulloa@gmail.com>
2013-09-24 16:14:00 +0000

    Modified solved problem 3

--- p3.R
+++ p3.R
@@ -4,31 +4,19 @@
 PrimeFac <- function(number) {
     if (number > 1e15) stop ("number is too large")
     largest.factor <- c(0)
-    if (number < 1e5) {
-        while (largest.factor < number) {
-            for (i in 2:number) {
-                if (number %% i == 0) {
-                    number <- number / i
-                    largest.factor <- i
-                }
+    max.factor <- number%/%sqrt(number)
+    while (largest.factor < number) {
+        for (i in 2:max.factor) {
+            if (number %% i == 0) {
+                number <- number / i
+                largest.factor <- i
+                max.factor <- number
             }
         }
-        print(largest.factor)
-    } else {
-        max.factor <- number%/%sqrt(number)
-        while (largest.factor < number) {
-            for (i in 2:max.factor) {
-                if (number %% i == 0) {
-                    number <- number / i
-                    largest.factor <- i
-                    max.factor <- number
-                }
-            }
-        }
-        if (largest.factor == 0) {
-            largest.factor <- number
-        }
-        print(largest.factor)
     }
+    if (largest.factor == 0) {
+        largest.factor <- number
+    }
+    print(largest.factor)
 }
-PrimeFac(600851475143)
+PrimeFac(644658)
