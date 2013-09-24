00a49998a60fe00f8a39ebc1724ae4bdfaaf583d
ulloa <samuel.ulloa@gmail.com>
2013-09-24 02:28:53 +0000

    Added solved problem 2

--- p2.R
+++ p2.R
@@ -1,3 +1,15 @@
 ## http://projecteuler.net/problem=2
 ## sum of even valued terms, up to 4mil, in Fibonacci sequence beg 1, 2, 3, 5
 
+maximum <- 4000000
+odd1 <- 1
+odd2 <- 1
+even <- 2
+suma <- 0
+while (even  < maximum) {
+    suma <- even + suma
+    odd1 <- odd2 + even
+    odd2 <- even + odd1
+    even <- odd1 + odd2
+}    
+print(suma)
